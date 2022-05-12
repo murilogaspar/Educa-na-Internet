0f6e02fae2bc3ce7e5606f02f02ae90d2e46d48c
Murilo Gaspar <36924852+murilogaspar@users.noreply.github.com>
2022-05-12 18:34:45 -0300

    inserindo na pagina de contato libras

--- menumain.js/menuprincipalmain.js
+++ menumain.js/menuprincipalmain.js
@@ -1,14 +1,19 @@
 let menuIsOpened = false;
 
+/* Mapeando o botão nav ul.button que é o que está no css*/
 const button = document.querySelector('nav ul.button');
 
+/* Mapeando o botão nav ul.menu que é o que está no css*/
 const menu = document.querySelector('nav ul.menu');
 
+/* o button é do html , puxou o botão e adiciona o evento click */
 button.addEventListener("click", () => {
+    /* se o menu estiver com o display flex */
     if (!menuIsOpened){
         menu.style.display ='flex';
         menuIsOpened = true;
     } else{
+        /* se o menu estiver com o display none */
         menu.style.display = 'none';
         menuIsOpened = false;
     }
